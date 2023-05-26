c1fa868baaa2da68f295945a4428c39698803b77
Taras Makarchuk <2278452@gmail.com>
2023-05-26 16:32:39 +0300

    :white_check_mark: add routing to pages

--- client/src/components/layout/header/Header.tsx
+++ client/src/components/layout/header/Header.tsx
@@ -1,16 +1,22 @@
 import { FC } from 'react';
-import { Cart } from './cart/Cart';
-import { History } from './history/History';
-import { Coupons } from './coupons/Coupons';
+import { Link } from 'react-router-dom';
 import './header.css';
 
 export const Header: FC = () => {
 	return (
 		<header className='header'>
-			<div className='shop'>Shop</div>
-			<Cart />
-			<History />
-			<Coupons />
+			<Link to='/' className='link-to-page vertical-right-border'>
+				Shop
+			</Link>
+			<Link to='/cart' className='link-to-page vertical-right-border'>
+				Shopping Cart
+			</Link>
+			<Link to='/history' className='link-to-page vertical-right-border'>
+				History
+			</Link>
+			<Link to='/coupons' className='link-to-page'>
+				Coupons
+			</Link>
 		</header>
 	);
 };
